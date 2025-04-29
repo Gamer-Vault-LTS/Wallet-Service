9a8ec21c6c0a89600d846b6b7fa6dfe54553223e
Jason-19 <jasonnunez02016@gmail.com>
2025-04-28 20:00:42 -0500

    test pipeline ECS using Runner EC2

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ app.register_blueprint(wallet_bp, url_prefix="/wallet")
 @app.route('/')
 def index():
     return "Wallet service is up", 200
-# Ruta para comprobar la salud de la aplicación
+
+
+@app.route('/health')
+def health():
+    return 'OK', 200
+
 @app.route('/wallets/health')
 def health_check():
     try:
@@ -22,6 +27,8 @@ def health_check():
         return "OK - Database Connected wallet", 200
     except Exception as e:
         return f"Database Connection Failed (wallet): {str(e)}", 500
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
