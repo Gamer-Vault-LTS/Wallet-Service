b5ac78e07a0903035f6cb902b6ad16d1534df1d3
JeremyAizprua <jeremy.aizprua@utp.ac.pa>
2025-05-08 09:23:52 -0500

    Interes corregido

--- models/interest_hisstory.py
+++ models/interest_hisstory.py
@@ -5,9 +5,9 @@ from datetime import datetime
 class Interest(db.Model):
     __tablename__ = 'interest_history'
 
-    interest_id = db.Column(db.String(50),nullable=False)
+    interest_id = db.Column(db.String(50), primary_key=True, nullable=False, default=lambda: str(uuid.uuid4()))
     user_id = db.Column(db.String(50), nullable=False)
-    balance = db.Column(db.Numeric(10, 2), default=0.00)
-    interest_generated = db.Column(db.Numeric(10, 2), default=0.00)
-    annual_interest = db.Column(db.Numeric(10, 2), default=0.00)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    balance = db.Column(db.Numeric(10, 2), nullable=False, default=0.00)
+    interest_generated = db.Column(db.Numeric(10, 2), nullable=False, default=0.00)
+    annual_interest = db.Column(db.Numeric(10, 2), nullable=False, default=0.00)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
